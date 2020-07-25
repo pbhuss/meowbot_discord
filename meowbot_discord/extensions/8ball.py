bffa8ec0de83ae1a1b6640dd50566596cbde9c7e
Peter Huss <pbhuss@users.noreply.github.com>
2020-07-24 21:10:42 -0700

    Upgrade requirements. Add !roll command

--- meowbot_discord/extensions/8ball.py
+++ meowbot_discord/extensions/8ball.py
@@ -1,4 +1,5 @@
 import random
+import re
 
 from discord.ext import commands
 from discord.ext.commands import Context, Bot
@@ -14,5 +15,22 @@ async def _8ball(ctx: Context, *, question):
     await ctx.send(text)
 
 
+@commands.command(name="roll", help="Roll some dice")
+async def _roll(ctx: Context, *, roll):
+    result = re.search(r"(\d+)d(\d+)", roll)
+    if result is None:
+        await ctx.send("Usage: `!roll {x}d{y}`")
+        return
+
+    num_dice = int(result.group(1))
+    num_sides = int(result.group(2))
+    if num_dice > 100 or num_sides < 2:
+        await ctx.send("No.")
+        return
+    rolls = (random.randint(1, num_sides) for _ in range(num_dice))
+    await ctx.send(f"🎲 I rolled: {', '.join(map(str, rolls))}")
+
+
 def setup(bot: Bot):
     bot.add_command(_8ball)
+    bot.add_command(_roll)
